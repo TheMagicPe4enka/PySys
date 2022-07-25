14696650164da506f3a0aa401181984eec19fd85
TheMagicPe4enka <05maker05@gmail.com>
2022-07-25 21:06:16 +0700

    Something

---
+++ PySys.ConsoleFabric/CConsoleFabricTool.py
@@ -0,0 +1,10 @@
+class ConsoleFabricTool:
+    def __init__(self, pAppLog, pAppParam, pApp):
+        self.app_log = pAppLog
+        self.app_param = pAppParam
+        self.app_exe = pApp
+        self.onStart()
+        pass
+
+    def onStart(self):
+        pass
